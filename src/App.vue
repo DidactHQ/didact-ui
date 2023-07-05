d35478fbca0c17203b942c8ee86f9aa220f00495
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-07-05 16:13:23 -0500

    Wiped out DevExtreme. Moving to native VueJS and Flowbite going forward

--- src/App.vue
+++ src/App.vue
@@ -1,101 +1,85 @@
-<template>
-  <div id="root">
-    <div :class="cssClasses">
-      <component
-        :is="$route.meta.layout"
-        :title="title"
-        :is-x-small="screen.getScreenSizeInfo.isXSmall"
-        :is-large="screen.getScreenSizeInfo.isLarge"
-      >
-      <div class="content">
-        <router-view></router-view>
-      </div>
-        <template #footer>
-          <app-footer />
-        </template>
-      </component>
-    </div>
-  </div>
-</template>
-
-<script>
-import AppFooter from "./components/app-footer";
-import { sizes, subscribe, unsubscribe } from "./utils/media-query";
-import {
-  getCurrentInstance,
-  reactive,
-  onMounted,
-  onBeforeUnmount,
-  computed
-} from "vue";
-
-function getScreenSizeInfo() {
-  const screenSizes = sizes();
-
-  return {
-    isXSmall: screenSizes["screen-x-small"],
-    isLarge: screenSizes["screen-large"],
-    cssClasses: Object.keys(screenSizes).filter(cl => screenSizes[cl])
-  };
-}
-
-export default {
-  components: {
-    AppFooter
-  },
-  setup() {
-    const vm = getCurrentInstance();
-
-    const title = vm.proxy.$appInfo.title;
-    const screen = reactive({ getScreenSizeInfo: {} });
-    screen.getScreenSizeInfo = getScreenSizeInfo();
-    
-    function screenSizeChanged () {
-      screen.getScreenSizeInfo = getScreenSizeInfo();
-    }
-
-    onMounted(() => {
-      subscribe(screenSizeChanged);
-    });
-
-    onBeforeUnmount(() => {
-      unsubscribe(screenSizeChanged);
-    });
-
-    const cssClasses = computed(() => {
-      return ["app"].concat(screen.getScreenSizeInfo.cssClasses);
-    });
-
-    return {
-      title,
-      screen,
-      cssClasses
-    };
-  }
-};
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="scss">
-html,
-body {
-  margin: 0px;
-  min-height: 100%;
-  height: 100%;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-#root {
-  height: 100%;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
-* {
-  box-sizing: border-box;
-}
-
-.app {
-  @import "./themes/generated/variables.base.scss";
-  background-color: darken($base-bg, 5);
-  display: flex;
-  height: 100%;
+nav {
   width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
 }
 </style>
