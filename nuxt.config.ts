584d455ac6b19ca0f11b59e130cd4058aa8da292
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-03-20 03:42:18 -0500

    Added PrimeVue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 import tailwindcss from "@tailwindcss/vite";
+import Aura from '@primeuix/themes/aura';
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -11,5 +12,20 @@ export default defineNuxtConfig({
     ],
   },
   css: ['~/assets/css/main.css'],
+  modules: [
+    '@primevue/nuxt-module'
+  ],
+  primevue: {
+    options: {
+        theme: {
+            preset: Aura
+        }
+    },
+    components: {
+      // Need to temporarily exclude PrimeVue form components because of npm install bug.
+      // See https://github.com/primefaces/primevue/issues/7434.
+      exclude: ['Form', 'FormField']
+    }
+  },
   ssr: false
 })
