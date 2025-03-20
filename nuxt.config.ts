0df457b745be8d2cdb57c2d54379c54b1726c0d3
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-03-20 03:23:33 -0500

    Added Tailwind CSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ['~/assets/css/main.css'],
   ssr: false
 })
