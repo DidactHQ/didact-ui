fe391dd30df0a1515d6aa74cafbe6c4048e90c44
Daniel Miradakis <53345819+DMiradakis@users.noreply.github.com>
2025-03-23 18:11:12 -0500

    Added embedded csproj and nuget settings for appsettings files and wwwroot folder - This was a HUGE pain in the freaking butt. Finally done.

--- dotnet-app/DidactUi/Program.cs
+++ dotnet-app/DidactUi/Program.cs
@@ -1,6 +1,34 @@
+using DidactUi.Services;
 using Microsoft.Extensions.FileProviders;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Setup and bind the appsettings.
+
+var assembly = Assembly.GetExecutingAssembly();
+var assemblyName = assembly.GetName().Name;
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+// Support multi-environment appsettings files.
+var resourceFileName = string.IsNullOrEmpty(environment)
+    ? $"{assemblyName}.appsettings.json"
+    : $"{assemblyName}.appsettings.{environment}.json";
+
+// Fetch the appsettings.json file as an embedded resource.
+var stream = assembly.GetManifestResourceStream(resourceFileName);
+var reader = new StreamReader(stream!);
+var json = reader.ReadToEnd();
+
+// Create a new IConfiguration.
+var iConfiguration = new ConfigurationBuilder()
+    .AddJsonStream(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+    .Build();
+
+// Bind to the appsettings class.
+var appSettings = new AppSettings();
+iConfiguration.Bind(appSettings);
+
+#endregion
 
 // Add services to the container.
 
@@ -21,12 +49,17 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-app.UseDefaultFiles();
+
+// Use an embedded file provider for the embedded wwwroot folder.
+var embeddedFileProvider = new ManifestEmbeddedFileProvider(assembly, "wwwroot");
+app.UseDefaultFiles(new DefaultFilesOptions
+{
+    FileProvider = embeddedFileProvider
+});
 app.UseStaticFiles(new StaticFileOptions
 {
-    FileProvider = new PhysicalFileProvider(
-        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")
-    ),
-    RequestPath = ""
+    FileProvider = embeddedFileProvider,
+    RequestPath = string.Empty
 });
+
 app.Run();
